9757bc795e67f418c2e3128fc29d42a6f5d6794d
Augusto Abad <augusto.abad@outlook.com>
2026-01-08 22:04:39 +0100

    add move bronze to silver

--- src/bronze.py
+++ src/bronze.py
@@ -2,24 +2,24 @@ import os
 import pandas as pd
 from datetime import datetime
 from pathlib import Path
-from password import bronze_path, bronze_parquet, bronze_untrack_parquet, bronze_srt_parquet
-from src.utils.functions import get_title_year, get_logger
+from password import (bronze_path, silver_path, bronze_parquet,
+                      bronze_untrack_parquet, bronze_srt_parquet)
+from src.utils.functions import get_title_year, get_logger, move_file_to_silver
 from src.utils.api import get_tmdb_id, get_data
+
+# Inicio logger
+logger = get_logger(__name__)
 
 # Definición de variables temporales
 films = []
 untrack = []
-# df_untrack = pd.DataFrame(columns=["File", "Path"])
-# df_srt = pd.DataFrame(columns=["Film", "Subtitle", "Path"])
 subtitles = []
-logger = get_logger(__name__)
 
 for root_path, _, file_list in os.walk(bronze_path):
     for file_name in file_list:
         if file_name.lower().endswith((".mkv", ".avi")):
 
             # Obtención de TMDB_ID
-            logger.info(f"Procesando {file_name}")
             film_title, film_year = get_title_year(file_name)
             tmdb_id = get_tmdb_id(film_title, film_year)
 
@@ -29,14 +29,14 @@ for root_path, _, file_list in os.walk(bronze_path):
                 logger.warning(f"No se ha obtenido TMDB ID para: {file_name}")
                 dictio_untrack = {
                     "File": file_name,
-                    "Path": str(Path(root_path) / file_name)
+                    "Path": str(Path(root_path) / file_name),
+                    "Date" : datetime.now()
                 }
                 untrack.append(dictio_untrack)
 
                 continue
 
             # Obtención datos API
-            logger.info(f"TMDB ID: {tmdb_id}")
             dictio = get_data(tmdb_id)
             dictio["path"] = str(Path(root_path) / file_name)
             dictio["bronze_date"] = datetime.now()
@@ -45,7 +45,7 @@ for root_path, _, file_list in os.walk(bronze_path):
             # Registro de subtítulos para .avi
             if file_name.lower().endswith(".avi"):
                 for root_path_srt, dir_names, file_list_srt in os.walk(root_path):
-                    dir_names.clear() # Evita entrar en subcarpetas
+                    dir_names.clear() # Evita entrar en sub carpetas
                     for file_name_srt in file_list_srt:
                         if file_name_srt.lower().endswith(".srt"):
                             dictio_srt = {
@@ -57,11 +57,30 @@ for root_path, _, file_list in os.walk(bronze_path):
 
 
 # Escritura de tabla con la información de la API
-pd.DataFrame(films).to_parquet(bronze_parquet, engine="pyarrow")
+if films:
+    pd.DataFrame(films).to_parquet(bronze_parquet, engine="pyarrow")
+    # Bronze --> Silver
+    # Archivos
+    for _, row in pd.DataFrame(films).iterrows():
+        move_file_to_silver(
+            title=row["Titulo"],
+            source_path=row["path"],
+            silver_path=silver_path
+        )
+        logger.info(f"BRONZE --> SILVER: {row['TMDB_id']} - {row['Titulo']}")
 
 # Registro de subtítulos
 if subtitles:
     pd.DataFrame(subtitles).to_parquet(bronze_srt_parquet, engine="pyarrow")
+    # Bronze --> Silver
+    # Subtítulos
+    for _,row in pd.DataFrame(subtitles).iterrows():
+        move_file_to_silver(
+            title=row["Film"],
+            source_path=row["Path"],
+            silver_path=silver_path
+        )
+    logger.info("BRONZE --> SILVER: Subtítulos")
 
 # Registro de archivos no localizados en la API
 if untrack:
