8cf5f08c527a335fb721119d4d0c7cb9386ffc1c
Augusto Abad <augusto.abad@outlook.com>
2026-01-11 18:50:26 +0100

    add creación de campos

--- src/silver.py
+++ src/silver.py
@@ -1,3 +1,19 @@
 import pandas as pd
+import os
+from password import BRONZE_PARQUET, FILMS_PARQUET
+from src.utils.functions import get_logger, create_cod_letter
 
-df_bronze = pd.read_parquet(bronze_parquet)
+# Inicio logger
+logger = get_logger(__name__)
+
+# Carga tabla bronze para transformación silver
+df_silver = pd.read_parquet(BRONZE_PARQUET, engine="pyarrow")
+
+# Creación de campos
+df_silver = create_cod_letter(df_silver)
+
+# Obtención de índices
+df_index = pd.read_parquet(FILMS_PARQUET, engine="pyarrow")[["ID","COD_LETTER","COD_INDEX"]]
+
+
+
